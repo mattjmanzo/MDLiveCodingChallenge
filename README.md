7af4bda183d8cd954b142742b34d2b15603489e2
mattjmanzo <65504202+mattjmanzo@users.noreply.github.com>
2021-01-25 20:56:08 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# MDLiveCodingChallenge
+# MDLiveChallenge
+
+## Getting Started
+
+1. Install the latest version [Node.js](https://nodejs.org/en/) and confirm the installation was successful:
+
+```shell
+$ node -v
+  v.14.15.4
+```
+
+2. Step into the project directory and run the following command:
+
+```shell
+$ node index.js
+```
+
+3. Send POST requests to http://localhost:8080/apps with an optional JSON body of this form:
+
+```json
+{
+    "range": {
+        "by": "name",
+        "start": "my-app-001",
+        "end": "my-app-050",
+        "max": 10,
+        "order": "asc"
+    }
+}
+```
+
+## Author
+
+[Matt Manzo](https://github.com/mattjmanzo)
---
+++ index.js
@@ -0,0 +1,88 @@
+const http = require('http');
+const apps = require('./apps.json');
+
+const port = 8080;
+const errorMessage = 'Please only make POST requests to the /apps API endpoint with optional valid JSON';
+const defaultRange = {
+	by: "id",
+	startId: apps[0].id,
+	endId: apps[apps.length - 1].id,
+	startName: apps[0].name,
+	endName: apps[apps.length - 1].name,
+	max: 50,
+	order: "asc"
+};
+
+http.createServer(function (request, response) {
+	const { pathname } = new URL(request.url, `http://${request.headers.host}`);
+	console.log("Request received");
+	console.log(`Path name: ${pathname}`);
+
+	if (request.method !== 'POST') {
+		// Only acccept POST requests
+		// response.writeHead(405, {'Content-Type': 'text/plain'});
+		response.end(errorMessage);
+	} else if (pathname !== "/apps") {
+		// The API only has a single endpoint: /apps
+		// response.writeHead(404, {'Content-Type': 'text/html'});
+		response.end(errorMessage);
+	} else {
+		let body = "";
+		request.on('data', (chunk) => {
+			body += chunk;
+		});
+		request.on('end', () => {
+			console.log(`Body: ${body}`);
+
+			if (body) {
+				try {
+					body = JSON.parse(body);
+				} catch {
+					console.error("Request provided invalid JSON. Using default JSON instead");
+					body = "";
+				}
+			}
+
+			// Send paginated apps response
+			if (typeof body !== "object" || !body.range) {
+				console.log("no range");
+				response.writeHead(200, { 'Content-Type': 'application/json' });
+				response.end(JSON.stringify(getPaginatedApps(apps)));
+			} else {
+				if (!body.range.by) {
+					console.log("no by");
+					response.end('Missing required valid "by" value in "range" query param');
+				} else {
+					console.log("by");
+					response.writeHead(200, { 'Content-Type': 'application/json' });
+					response.end(JSON.stringify(getPaginatedApps(apps, body.range)));
+				}
+			}
+
+		});
+	}
+}).listen(port, () => {
+	console.log(`Server running on port ${port}`);
+});
+
+/** Returns a subset of consecutive objects from `data` according to `range` */
+const getPaginatedApps = (data, range) => {
+
+	// Select user-defined or default range parameters
+	const byValue = range?.by || defaultRange.by;
+	const startValue = range?.start || (byValue === "id" ? defaultRange.startId : defaultRange.startName);
+	const endValue = range?.end || (byValue === "id" ? defaultRange.endId : defaultRange.endName);
+	const maxValue = range?.max || defaultRange.max;
+	const orderValue = range?.order || defaultRange.order;
+
+	// Find the slice indices
+	const startIndex = data.findIndex(({ id, name }) => (byValue === "id" ? id : name) === startValue);
+	let endIndex = data.findIndex(({ id, name }) => (byValue === "id" ? id : name) === endValue);
+	endIndex = maxValue <= endIndex - startIndex ? startIndex + maxValue : endIndex + 1;
+
+	// Slice (paginate) the data
+	const slicedData = data.slice(startIndex, endIndex);
+
+	// Return slice data ordered appropriately
+	return orderValue === "asc" ? slicedData : slicedData.reverse();
+};
